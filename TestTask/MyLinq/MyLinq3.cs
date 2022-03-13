db83ae5cce48a73928a703796b4e6bc522a285fa
VsevolodGus <gusakseva8@gmail.com>
2022-03-13 18:05:40 +0500

    перевел на бирнаый поиск

--- TestTask/MyLinq/MyLinq3.cs
+++ TestTask/MyLinq/MyLinq3.cs
@@ -1,52 +1,53 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-//using System.Linq; // только для ToArray в MyContains
+using System.Linq; // только для ToArray в MyContains
 
 namespace TestTask.MyLinq
 {
     public static partial class MyLinq
     {
 
-        //TODO: если отсортировано по датам - бинарный поиск
+        // делаю бинарник т.к файл отсортирован
         public static bool MyContains(this IEnumerable<Bar> bars, Bar searchElement)
         {
-            #region 
-            //var mainBars = bars.ToArray();
-            //var beginIndex = 0;
-            //var endIndex = mainBars.Length - 1;
+            #region Бинарный
+            var mainBars = bars.ToArray();
+            var beginIndex = 0;
+            var endIndex = mainBars.Length - 1;
 
-            //while (beginIndex < endIndex)
+            while (beginIndex < endIndex)
+            {
+                var middleIndex = (beginIndex + endIndex) / 2;
+
+                if (mainBars[middleIndex].Symbol == searchElement.Symbol
+                        && mainBars[middleIndex].Description == searchElement.Description
+                        && mainBars[middleIndex].DateTime == searchElement.DateTime)
+                    return true;
+
+                if (middleIndex == 0 || middleIndex == mainBars.Length - 1)
+                    return false;
+
+                if (mainBars[middleIndex].DateTime <= searchElement.DateTime)
+                {
+                    beginIndex = middleIndex + 1;
+                }
+                else
+                {
+                    endIndex = middleIndex;
+                }
+            }
+            #endregion
+
+            #region Перебор
+            //foreach (var item in bars)
             //{
-            //    var middleIndex = (beginIndex + endIndex) / 2;
-
-            //    if (mainBars[middleIndex].Symbol == searchElement.Symbol
-            //            && mainBars[middleIndex].Description == searchElement.Description
-            //            && mainBars[middleIndex].DateTime == searchElement.DateTime)
+            //    if (item.Symbol == searchElement.Symbol
+            //            && item.Description == searchElement.Description
+            //            && item.DateTime == searchElement.DateTime)
+            //    {
             //        return true;
-
-            //    if (middleIndex == 0 || middleIndex == mainBars.Length - 1)
-            //        return false;
-
-            //    if (mainBars[middleIndex].DateTime <= searchElement.DateTime)
-            //    {
-            //        beginIndex = middleIndex + 1;
-            //    }
-            //    else
-            //    {
-            //        endIndex = middleIndex;
             //    }
             //}
             #endregion
-
-            foreach (var item in bars)
-            {
-                if (item.Symbol == searchElement.Symbol
-                        && item.Description == searchElement.Description
-                        && item.DateTime == searchElement.DateTime)
-                {
-                    return true;
-                }
-            }
 
             return false;
         }
@@ -82,7 +83,6 @@ namespace TestTask.MyLinq
 
         public static List<Bar> Intersection(this IEnumerable<Bar> thisBars, IEnumerable<Bar> otherBars)
         {
-            thisBars = thisBars.ToArray();
             var result = new List<Bar>();
 
             foreach (var item in thisBars)
