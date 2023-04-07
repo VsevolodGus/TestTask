e71f6b1376841684b70d2f0abad556d4d2a5e312
VsevolodGus <gusakseva8@gmail.com>
2023-04-08 04:50:19 +0500

    add comments

--- TestTask/SpaceService.cs
+++ TestTask/SpaceService.cs
@@ -27,9 +27,52 @@ public static class SpaceService
     {
         var node = new Node(point);
         rootNodes.Add(node);
-
+        var maxX = space.Length;
+        var maxY = space.Min(c=> c.Length);
         //TODO добавить логику 
         // нужно добавлять к узлу соседние элементы, делая проверку на кол-во  на ограничения на границы массива
         // делать это в цикле, не в рекурсии
+
+        //up [x][y-1]
+        //left [x-1][y]
+        //right [x+1][y]
+        //down [x][y+1]
+        // up left corner [x-1][y-1]
+        // up right corner [x+1][y-1]
+        // down left corner [x-1][y+1]
+        // down right corner [x+1][y+1]
+
+        do
+        {
+            if (point.X == 0)
+            {
+                // do not left [x-1][y]
+                // do not up left corner [x-1][y-1]
+                // do not down left corner [x-1][y+1]
+            }
+
+            if (point.Y == 0)
+            {
+                // do not up [x][y-1]
+                // do not up left corner [x-1][y-1]
+                // do not up right corner [x+1][y-1]
+            }
+
+            if (point.X + 1 == maxX)
+            {
+                // do not left [x-1][y]
+                // do not up left corner [x-1][y-1]
+                // do not down left corner [x-1][y+1]
+            }
+
+            if (point.Y + 1 == maxY)
+            {
+                // do not down [x][y+1]
+                // do not down left corner [x-1][y+1]
+                // do not down right corner [x+1][y+1]
+            }
+
+        }while(false);
+
     }
 }
